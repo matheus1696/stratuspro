8650854344b523a10b9450b0b186da4d730b7c97
Matheus Mota <matheus.mat1996@gmail.com>
2025-01-16 10:44:07 -0300

    StratusPRO V1.0 - Managenment Contracts

--- resources/views/livewire/managenment/business/contract/contract-show.blade.php
+++ resources/views/livewire/managenment/business/contract/contract-show.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-pages.conteiner class="text-sm">
         @if ($dbContract->status_id === $dbContractStatus->id)
-            <div class="flex justify-end">
+            <div class="flex flex-col justify-center items-end gap-3">
                 <a href="{{ route('contracts.edit', $dbContract->id) }}" class="bg-yellow-500 hover:bg-yellow-600 text-gray-800 hover:text-gray-900 rounded-full shadown-lg text-xs transition-all duration-300 px-2.5 py-2 absolute">
                     <i class="fas fa-pen"></i>
                 </a>
