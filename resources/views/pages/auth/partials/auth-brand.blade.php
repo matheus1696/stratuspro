773a316274e16c1f10d3dcbf9e9742d2c9c46a5e
Matheus Mota <matheus.mat1996@gmail.com>
2025-02-12 18:24:21 -0300

    StratusPro V1.0 - Warehouse

--- resources/views/pages/auth/partials/auth-brand.blade.php
+++ resources/views/pages/auth/partials/auth-brand.blade.php
@@ -1,5 +1,6 @@
 <div class="text-center mb-6">
-    <div class="flex justify-center items-center text-2xl gap-2">
+    <div class="flex justify-center items-center text-3xl">
+        <div><img src="assets/img/logo.png" alt="Logo StratusPRO" class="size-12"></div>
         <p class="font-semibold text-white"> {{ env('APP_NAME') }} </p>
     </div>
 </div>
--- resources/views/pages/auth/partials/auth-illustration.blade.php
+++ resources/views/pages/auth/partials/auth-illustration.blade.php
@@ -1,1 +1,2 @@
 <img src="assets/img/illustration/{{ rand(1,8) }}.png" alt="Ilustração" class="w-full lg:w-60">
+<p class="text-blue-400 font-semibold text-[12px] mt-4">Desenvolvido por Matheus Mota</p>
